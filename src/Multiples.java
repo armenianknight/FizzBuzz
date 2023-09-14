48a701c3fdce3eb79e6fc572862e156769da38c3
Vic <supaspartam@gmail.com>
2023-09-14 19:31:04 -0400

    Finished version of multiples that now counts how many multiples of 3 and 5 there are that are less than 1000, excluding 1000

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,31 +1,34 @@
 public class Multiples {
     public static void main(String[] args) {
         System.out.println(domultiples(1000));
+        //System.out.println(whilemultiple(1000)); This was just for the while loop
     }
 
     public static int domultiples(int n) {
-        //attempt at a while loop
-        /*
-        int i = 1;
-        boolean multiple_of_3 = i % 3 == 0;
-        boolean multiple_of_5 = i % 5 == 0;
-        int count = 0;
-        while (i <= n) {
-            if (multiple_of_3 || multiple_of_5) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-         */
         //for loop
         int count = 0;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i < n; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
                 count++;
             }
         }
         return count;
     }
+    /* Finished while loop version
+    public static int whilemultiple(int m) {
+        int count = 0;
+        int number = 1;
+
+        while (number < m) {
+            if (number % 3 == 0 || number % 5 == 0) {
+                count++;
+            }
+            number++;
+        }
+
+        return count;
+    }
+
+    */
 }
+
